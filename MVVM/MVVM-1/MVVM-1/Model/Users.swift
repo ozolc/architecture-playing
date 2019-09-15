0db8438d44472d8abde3a2dac73a1a9c92009923
Maksim Nosov <ozolc@yandex.ru>
2019-09-15 16:45:23 +0300

    Изменена структура модели для Users

--- MVVM/MVVM-1/MVVM-1/Model/Users.swift
+++ MVVM/MVVM-1/MVVM-1/Model/Users.swift
@@ -10,23 +10,10 @@ import Foundation
 
 public struct Users: Codable {
     let results: [Result]?
-    let info: Info?
-}
-
-struct Info: Codable {
-    let seed: String?
-    let results, page: Int?
-    let version: String?
 }
 
 struct Result: Codable {
-//    let gender: Gender?
     let name: NameClass?
-//    let email: String?
-//    let login: Login?
-//    let phone, cell: String?
-//    let id: ID?
-//    let picture: Picture?
     
     enum CodingKeys: String, CodingKey {
         case name
@@ -37,11 +24,6 @@ struct Result: Codable {
         
         self.name = try? container.decode(NameClass.self, forKey: .name)
     }
-}
-
-enum Gender: String, Codable {
-    case female
-    case male
 }
 
 struct NameClass: Codable {
@@ -57,33 +39,4 @@ enum Title: String, Codable {
     case mr
     case mrs
     case ms
-}
-
-struct Login: Codable {
-    let uuid, username, password, salt: String?
-    let md5, sha1, sha256: String?
-}
-
-struct ID: Codable {
-    let name: NameEnum?
-    let value: String?
-}
-
-enum NameEnum: String, Codable {
-    case avs = "AVS"
-    case bsn = "BSN"
-    case cpr = "CPR"
-    case dni = "DNI"
-    case empty = ""
-    case fn = "FN"
-    case hetu = "HETU"
-    case insee = "INSEE"
-    case nino = "NINO"
-    case pps = "PPS"
-    case ssn = "SSN"
-    case tfn = "TFN"
-}
-
-struct Picture: Codable {
-    let large, medium, thumbnail: String?
 }
